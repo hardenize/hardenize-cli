d893b7a8457233bf93a9c9723405da9d98f29b60
Mike Cardwell <mike.cardwell@hardenize.com>
2018-08-20 17:15:06 +0100

    Nicer error when trying to retrieve a cert that doesn't exist

--- command/get_cert.js
+++ command/get_cert.js
@@ -3,6 +3,12 @@ module.exports = function get_cert(sha256) {
   return this.api().getCert(sha256)
     .then(function(response){
       console.log(JSON.stringify(response.data, null, 2));
+    })
+    .catch(function(err){
+      if (err.res && err.res.status === 404) {
+        return console.warn('Certificate not found');
+      }
+      return Promise.reject(err);
     });
 
 };
