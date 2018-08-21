5c10384f45b5b7190bbc2163326cc365dee96c59
Mike Cardwell <mike.cardwell@hardenize.com>
2018-08-21 17:06:37 +0100

    Fixes for filtering on certs ls

--- src/cmds/certs_cmds/ls.js
+++ src/cmds/certs_cmds/ls.js
@@ -5,12 +5,12 @@ exports.command = 'ls';
 exports.desc = 'List certificates';
 
 exports.builder = function(yargs) {
-    yargs.option('active',                { type: 'boolean', description: 'Return only active cerificates' });
-    yargs.option('expired',               { type: 'boolean', description: 'Return only expired certificates' });
-    yargs.option('expire-in-days <days>', { type: 'number',  description: 'Return only certificates that have already expired or expire in the specified number of days, according to the effectiveNotAfter timestamp' });
-    yargs.option('host <host>',           { type: 'string',  description: 'Return only certificates that are valid for the specified host, either because they contain the exact hostname or because they are wildcards and contain the parent hostname (e.g., a search for blog.example.com will match *.example.com wildcards)' });
-    yargs.option('limit <limit>',         { type: 'number',  description: 'Return only up to the specified number of certificates' });
-    yargs.option('spki-sha256 <sha256>',  { type: 'string',  description: 'Return only certificates whose public key (SPKI) matches the provided hash' });
+    yargs.option('active',         { type: 'boolean', description: 'Return only active cerificates' });
+    yargs.option('expired',        { type: 'boolean', description: 'Return only expired certificates' });
+    yargs.option('expire-in-days', { type: 'number',  description: 'Return only certificates that have already expired or expire in the specified number of days, according to the effectiveNotAfter timestamp' });
+    yargs.option('host',           { type: 'string',  description: 'Return only certificates that are valid for the specified host, either because they contain the exact hostname or because they are wildcards and contain the parent hostname (e.g., a search for blog.example.com will match *.example.com wildcards)' });
+    yargs.option('limit',          { type: 'number',  description: 'Return only up to the specified number of certificates' });
+    yargs.option('spki-sha256',    { type: 'string',  description: 'Return only certificates whose public key (SPKI) matches the provided hash' });
 }
 
 exports.handler = function ls_certs_handler(argv) {
