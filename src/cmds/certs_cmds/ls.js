4cd3b96f055ac921388450be4036f3a989f8c14f
Mike Cardwell <mike.cardwell@hardenize.com>
2018-09-06 14:03:22 +0100

    certs ls gets a --new-since option

--- src/cmds/certs_cmds/ls.js
+++ src/cmds/certs_cmds/ls.js
@@ -9,6 +9,7 @@ exports.builder = function(yargs) {
     yargs.option('expired',        { type: 'boolean', description: 'Return only expired certificates' });
     yargs.option('expire-in-days', { type: 'number',  description: 'Return only certificates that have already expired or expire in the specified number of days, according to the effectiveNotAfter timestamp' });
     yargs.option('limit',          { type: 'number',  description: 'Return only up to the specified number of certificates' });
+    yargs.option('new-since',      { type: 'date',    description: 'Return only certificates added to the account since the specified date' });
     yargs.option('spki-sha256',    { type: 'string',  description: 'Return only certificates whose public key (SPKI) matches the provided hash' });
 }
 
@@ -22,6 +23,7 @@ exports.handler = function ls_certs_handler(argv) {
     if (argv.hasOwnProperty('expire-in-days')) opt.expireInDays = argv['expire-in-days'];
     if (argv.hasOwnProperty('host'))           opt.host         = argv.host;
     if (argv.hasOwnProperty('limit'))          opt.limit        = argv.limit;
+    if (argv.hasOwnProperty('new-since'))      opt.newSince     = argv['new-since'];
     if (argv.hasOwnProperty('spki-sha256'))    opt.spkiSha256   = argv['spki-sha256'];
 
     api.init(argv).getCerts(opt)
