4854e14c00d7e48b282b3c744597895fdaaf64d6
Mike Cardwell <mike.cardwell@hardenize.com>
2018-09-10 11:47:44 +0100

    Support --display-name on tag creation

--- src/cmds/tags_cmds/add.js
+++ src/cmds/tags_cmds/add.js
@@ -1,12 +1,19 @@
 var api = require('../../api');
 
-exports.command = 'add <tag>';
+exports.command = 'add <name>';
 
 exports.desc = 'Add a tag';
 
+exports.builder = function(yargs) {
+    yargs.option('display-name', { type: 'string', description: 'Display name (if different from name)' });
+}
+
 exports.handler = function add_tag_handler(argv) {
 
-    api.init(argv).addTag(argv.tag)
+    var options = {};
+    if (argv.hasOwnProperty('displayName')) options.displayName = argv.displayName;
+
+    api.init(argv).addTag(argv.name, options)
         .then(function(){
             console.log('Tag added');
         })
