adca4566e8a81edddfa38bde6ce6bc6a9ff33257
Mike Cardwell <mike.cardwell@hardenize.com>
2018-09-10 11:55:18 +0100

    Update --origin choices when filtering hosts

--- src/cmds/hosts_cmds/ls.js
+++ src/cmds/hosts_cmds/ls.js
@@ -7,8 +7,8 @@ exports.desc = 'List hosts';
 exports.builder = function(yargs) {
     yargs.option('subdomains', { type: 'boolean', description: 'Include all subdomains of the specified name', default: true });
     yargs.option('limit',      { type: 'integer', description: 'Return only up to the specified number of hosts' });
-    yargs.option('origin',     { type: 'string',  description: 'Return only hosts with this origin', choices: ['manual', 'ct', 'discovery'] });
-    yargs.option('status',     { type: 'string',  description: 'Return only hosts with this status', choices: ['idle', 'monitored', 'archived'] });
+    yargs.option('origin',     { type: 'string',  description: 'Return only hosts with this origin', choices: ['cert', 'ct', 'manual'] });
+    yargs.option('status',     { type: 'string',  description: 'Return only hosts with this status', choices: ['archived', 'idle', 'monitored'] });
     yargs.option('tag',        { type: 'string',  description: 'Return only hosts with this tag' });
 }
 
