d5c33a7267e50a91ae0ff26060fae66214ce6132
Mike Cardwell <mike.cardwell@hardenize.com>
2018-09-18 10:21:56 +0100

    suborgs update - Add --generate-api-credentials option

--- src/cmds/suborgs_cmds/update.js
+++ src/cmds/suborgs_cmds/update.js
@@ -5,14 +5,21 @@ exports.command = 'update <id>';
 exports.desc = 'Update organization';
 
 exports.builder = function(yargs) {
-    yargs.option('status', { type: 'string',  description: 'Organization status', choices: ['active', 'dormant', 'suspended', 'deleted'], required: true });
+    yargs.option('status',                   { type: 'string',  description: 'Organization status', choices: ['active', 'dormant', 'suspended', 'deleted'] });
+    yargs.option('generate-api-credentials', { type: 'boolean', description: 'Generates and returns new API credentials. They will need recording as you wont be able to view them again' });
 }
 
 exports.handler = function update_suborg_handler(argv) {
 
-    var options = {
-        status: argv.status,
-    };
+    var options = {};
+
+    if (argv.status)                 options.status                 = argv.status;
+    if (argv.generateApiCredentials) options.generateApiCredentials = true;
+
+    if (Object.keys(options).length === 0) {
+        console.error('Must specify one or more of --status and --generate-api-credentials');
+        process.exit(1);
+    }
 
     api.init(argv).updateSubOrg(argv.id, options)
         .then(function(response){
