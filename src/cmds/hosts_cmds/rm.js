3e0beb044894031e8c3f97c26a4a3be11304309a
Mike Cardwell <mike.cardwell@redsift.io>
2023-11-27 14:10:34 +0000

    Support --subdomains option for hosts rm

--- src/cmds/hosts_cmds/rm.js
+++ src/cmds/hosts_cmds/rm.js
@@ -5,13 +5,15 @@ exports.command = 'rm <hostnames...>';
 exports.desc = 'Delete hosts';
 
 exports.builder = function(yargs) {
-    yargs.option('preview', { type: 'boolean', description: 'Do not apply changes. Just return a list of hosts that would have been affected' });
+    yargs.option('preview',    { type: 'boolean', description: 'Do not apply changes. Just return a list of hosts that would have been affected' });
+    yargs.option('subdomains', { type: 'boolean', description: 'Delete subdomains too' });
 }
 
 exports.handler = function rm_hosts_handler(argv) {
 
     var options = {};
-    if (argv.preview) options.preview = true;
+    if (argv.preview)    options.preview    = true;
+    if (argv.subdomains) options.subdomains = true;
 
     cmd.api(argv).deleteHosts(argv.hostnames, options)
         .then(function(response){
